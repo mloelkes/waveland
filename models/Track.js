9d1b4140aab65664b7c427da6cf14978b216f755
Marco Lölkes <marcoloelkes@gmx.de>
2022-05-11 11:29:09 +0200

    add models

---
+++ models/Track.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const trackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    tag: String,
+    description: String,
+    link: String,
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    comments: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        description: String
+      }
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+          ref: 'User',
+      }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Track = model("Track", trackSchema);
+
+module.exports = Track;
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    image: String,
+    location: String,
+    description: String,
+    tracks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Track'
+      }
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Track'
+      }
+    ],
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
