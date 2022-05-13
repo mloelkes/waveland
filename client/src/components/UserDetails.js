44845a88be0f3446735bfb3fa559c6c3421cce68
Marco Lölkes <marcoloelkes@gmx.de>
2022-05-13 13:24:40 +0200

    Remove unused code and imports in Dashboard and userDetails

--- client/src/components/UserDetails.js
+++ client/src/components/UserDetails.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { UserDetailsContext } from "../context/userDetails.js";
 
 function UserDetails() {
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,25 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import UserDetails from "../components/UserDetails.js";
 
 function Dashboard() {
-
-
-
-    
-    
-
 	return (
-        
         <div className="Dashboard">
-
             <UserDetails></UserDetails>
             <Link to="/upload"><button className="primary-button">Upload a track</button></Link>
-
-		 
-
-
-
         </div>
 	)
 }
