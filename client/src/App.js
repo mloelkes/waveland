4807f0f771d880651798a86ac12162defef9ee12
Marco Lölkes <marcoloelkes@gmx.de>
2022-05-31 13:56:23 +0200

    Implement protection for backend and frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Upload from "./pages/Upload";
 import Profile from "./pages/Profile";
 import Player from "./components/Player";
 import Waves from "./pages/Waves";
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     const [trackUrl, setTrackUrl] = useState("");
@@ -31,10 +32,26 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/upload" element={<Upload />} />
-                <Route path="/waves" element={<Waves handlePlayTrack={handlePlayTrack}/>} />
-                <Route path="/:nameForUrl" element={<Profile handlePlayTrack={handlePlayTrack}/>} />
+                <Route path="/dashboard" element={
+                    <ProtectedRoute redirectTo='/login'>
+                        <Dashboard />
+                    </ProtectedRoute>
+                } />
+                <Route path="/upload" element={
+                    <ProtectedRoute redirectTo='/login'>
+                        <Upload />
+                    </ProtectedRoute>
+                } />
+                <Route path="/waves" element={
+                    <ProtectedRoute redirectTo='/login'>
+                        <Waves handlePlayTrack={handlePlayTrack}/>
+                    </ProtectedRoute>
+                } />
+                <Route path="/:nameForUrl" element={
+                    <ProtectedRoute redirectTo='/login'>
+                        <Profile handlePlayTrack={handlePlayTrack}/>
+                    </ProtectedRoute>
+                } />
             </Routes>
             <Player trackUrl={trackUrl} trackImage={trackImage} trackName={trackName} artistName={artistName}></Player>
         </div>
